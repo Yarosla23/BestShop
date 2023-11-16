1b1129b0826a31ec1c29333eb6ab315883bc6300
Yarosla23 <ayroslav56@gmail.com>
2023-11-16 22:53:44 +0300

    gem devise and profile

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :last_name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 150 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :photo, length: { maximum: 150 }
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 14, allow_blank: true }
+  validates :sity, length: { maximum: 150 }
+end
---
+++ db/migrate/20231116133811_add_description_to_users.rb
@@ -0,0 +1,9 @@
+class AddDescriptionToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :photo, :string
+    add_column :users, :age, :integer
+    add_column :users, :sity, :string
+  end
+end
