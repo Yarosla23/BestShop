1fa48231443a6b71f7c768a295e0d9de77e40ade
Yarosla23 <ayroslav56@gmail.com>
2023-11-23 22:37:42 +0300

    добавлен путь к странице профиля, сделан дизайн карточки продукта

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
   root 'home#index'
+  get '/users/profile', to: 'users#profile'
   devise_for :users, controllers: { registrations: 'users/registrations' }
 end
