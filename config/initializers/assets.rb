a36d2fcb31a0a804de0b8e4e7d73c0544838ccfd
Yarosla23 <ayroslav56@gmail.com>
2023-12-03 00:25:08 +0300

    добавлен новый navbar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( navbar.css product.css  profile.css light.css dark.css )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
